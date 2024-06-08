c1894579f5aeddc663789fcd9e8db769a08a111f
CGAJAY <jmugendi737@gmail.com>
2024-06-08 14:57:34 +0300

    rendered the word searched in the DOM

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-  .then((result) => {
-    return result.json();
-  })
-  .then((data) => console.log(data));
+let textInput = document.getElementById("text-inpt");
+// textInput.value = "mind";
+const searchBtn = document.getElementById("search-btn");
+const contentCont = document.querySelector(".content");
+const wordContDiv = document.createElement("div");
+wordContDiv.classList.add("word-cont");
+const searchedWord = document.createElement("h2");
+
+function findMeaning() {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${textInput.value}`)
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      searchedWord.textContent = textInput.value;
+      wordContDiv.appendChild(searchedWord);
+      contentCont.appendChild(wordContDiv);
+    });
+}
+
+searchBtn.addEventListener("click", findMeaning);
