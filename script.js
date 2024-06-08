0f6b3118094c0e480f7d8922a5b1f76d24ac188a
CGAJAY <jmugendi737@gmail.com>
2024-06-08 20:36:04 +0300

    rendered mmeanings of the word in the DOM

--- script.js
+++ script.js
@@ -8,23 +8,36 @@ const searchedWord = document.createElement("h2");
 const wordGreek = document.createElement("p");
 wordGreek.classList.add("italic");
 const languageCont = document.querySelector(".language-cont");
+const wordType = document.createElement("strong");
+const MeaningList = document.createElement("ol");
+const Meaning = document.createElement("li");
 
-function findMeaning() {
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${textInput.value}`)
-    .then((result) => {
-      return result.json();
-    })
-    .then((data) => {
-      searchedWord.textContent = textInput.value;
-      wordContDiv.appendChild(searchedWord);
-      wordGreek.textContent = `${data[0].phonetic}`;
-      wordContDiv.appendChild(wordGreek);
-      contentCont.insertBefore(
-        wordContDiv,
-        contentCont.firstElementChild.nextSibling
-      );
-      console.log(data);
+async function findMeaning() {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${textInput.value}`
+    );
+    const data = await response.json();
+    searchedWord.textContent = textInput.value;
+    wordContDiv.appendChild(searchedWord);
+    wordGreek.textContent = `${data[0].phonetic}`;
+    wordContDiv.appendChild(wordGreek);
+    contentCont.appendChild(wordContDiv);
+    wordType.textContent = `${data[0].meanings[0].partOfSpeech}`;
+    contentCont.appendChild(wordType);
+    console.log(data[0]);
+    //   console.log(data[0].meanings[0]);
+    //   console.log(data[0].meanings[0].definitions);
+    data[0].meanings[0].definitions.forEach((define) => {
+      console.log(define);
+      MeaningList.innerHTML += `<li>${define.definition}</li>`;
     });
+    contentCont.appendChild(MeaningList);
+  } catch (error) {
+    let message = document.createElement("h2");
+    message.textContent = `Input only english words`;
+    contentCont.appendChild(message);
+  }
 }
 
 searchBtn.addEventListener("click", findMeaning);
